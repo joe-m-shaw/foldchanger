5b44abc4b2cef4827f764f3ced0532a95853ae85
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-01-16 16:33:05 +0000

    Add vignette and update dependencies

--- R/calc_log2r.R
+++ R/calc_log2r.R
@@ -1,5 +1,15 @@
 calc_log2r <- function(obs, exp) {
 
+  #' Calculate log2 ratios
+  #'
+  #' @param obs Observed reads
+  #' @param exp Expected reads
+  #'
+  #' @return Log2 ratio
+  #' @export
+  #'
+  #' @examples calc_log2r(10, 5)
+
   log2 <- log2(obs/exp)
 
   return(log2)
