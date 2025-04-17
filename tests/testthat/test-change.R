abe1040d7cdc2ec4597ea4aa4a78181736ac4e18
joeshaw824 <57156322+joeshaw824@users.noreply.github.com>
2025-04-17 19:12:58 +0100

    Add tests

--- tests/testthat/test-change.R
+++ tests/testthat/test-change.R
@@ -7,3 +7,18 @@ test_that("change works from log2r to fcloss", {
   expect_equal(change(-1, from = "log2r", to = "fcloss"),
                -2)
 })
+
+test_that("change works from fcgain to fcloss", {
+  expect_equal(change(1, from = "fcgain", to = "fcloss"),
+               -1)
+})
+
+test_that("change works from fcgain to log2r", {
+  expect_equal(change(2, from = "fcgain", to = "log2r"),
+               1)
+})
+
+test_that("change works from fcloss to log2r", {
+  expect_equal(change(-2, from = "fcloss", to = "log2r"),
+               -1)
+})
--- tests/testthat/test-pred-fc.R
+++ tests/testthat/test-pred-fc.R
@@ -17,3 +17,13 @@ test_that("pred_fc works for a biallelic loss", {
   expect_equal(pred_fc(tumour_cpc = 0, ncc = 100, dir = "loss"),
                -Inf)
 })
+
+test_that("pred_fc works for gains with 0% NCC", {
+  expect_equal(pred_fc(tumour_cpc = 3, ncc = 0, dir = "gain"),
+               1)
+})
+
+test_that("pred_fc works for losses with 0% NCC", {
+  expect_equal(pred_fc(tumour_cpc = 1, ncc = 0, dir = "loss"),
+               -1)
+})
