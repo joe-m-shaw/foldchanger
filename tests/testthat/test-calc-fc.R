0afbb7acea7e39177a7cf4ab84c30d43371e14fc
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-04-17 16:01:21 +0100

    Add more tests for calc_fc

--- tests/testthat/test-calc-fc.R
+++ tests/testthat/test-calc-fc.R
@@ -1,5 +1,10 @@
-test_that("calc_fc works", {
-  expect_equal(calc_fc(obs = 10, exp = 2, dir = "gain"), 5)
+test_that("calc_fc works for gains", {
+  expect_equal(calc_fc(obs = 10, exp = 2, dir = "gain"),
+               5)
 })
 
+test_that("calc_fc works for loss", {
+  expect_equal(calc_fc(obs = 1, exp = 2, dir = "loss"),
+               -2)
+})
 
